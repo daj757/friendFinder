ec4aa55ee243c7fe491458143abd1afea6288ba3
Danny <DJ@MacBook-Pro.local>
2017-08-11 21:26:06 -0600

    survery

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,23 +2,23 @@
 let data = {
  current: [{
  	name: "John",
- 	photo: "",
+ 	photo: "http://assets3.thrillist.com/v1/image/1299823/size/tl-horizontal_main/7-weird-stock-images-of-people-struggling-with-basic-cooking",
  	scores: [1, 4, 3, 2, 5, 3, 2, 3, 3, 1]
  }],
  all : [
  {
  	name: "Edna",
- 	photo: "",
+ 	photo: "https://www.cityfalcon.com/blog/wp-content/uploads/2015/11/Dollarphotoclub_62733081_1240-945x629.jpg",
  	scores: [2, 4, 5, 1, 2, 4, 5, 1, 3, 5]
  },
  {
  	name: "Mike",
- 	photo: "",
+ 	photo: "http://www.designzzz.com/wp-content/uploads/2012/12/freestock_image_by_goldman555-d495w4s.jpg",
  	scores: [5, 2, 5, 2, 5, 3, 3, 4, 3, 5]
  },
  {
  	name: "Marie",
- 	photo: "",
+ 	photo: "https://leadershipspirit.files.wordpress.com/2013/10/stock_2013_hi-res-download_2_1.jpg",
  	scores: [2, 4, 3, 1, 5, 3, 1, 5, 2, 1]
  }]
 };
